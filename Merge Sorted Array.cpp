a36556cbb6bccedb8ba55dc1b568fe1e3a413c93
aniket-471 <aniket472001@gmail.com>
2022-05-30 08:17:01 +0530

    Create Merge Sorted Array.cpp

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0 )
+        {
+            if(n1[i]<=n2[j])
+                n1[k--]=n2[j--];
+            
+            else
+                n1[k--]=n1[i--];
+        }
+        while(i>=0)
+            n1[k--]=n1[i--];
+        while(j>=0)
+            n1[k--]=n2[j--];
+    }
+};
